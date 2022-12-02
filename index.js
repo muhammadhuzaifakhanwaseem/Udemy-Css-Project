ddd916ffdc07a039a51e1a5565c7308c49a3a6db
Muhammad Huzaifa Waseem <118267008+muhammadhuzaifakhanwaseem@users.noreply.github.com>
2022-12-02 19:04:16 +0500

    them change work update

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 let head = document.querySelector(".main-header");
 let art1 = document.querySelector(".art1");
-let art2 = document.querySelector(".art2");
 let art3 = document.querySelector(".art3");
 let card = document.getElementById("packages");
 function scroll() {
@@ -20,18 +19,18 @@ function scroll() {
 function scroll() {
   if (window.scrollY >= 400) {
     art1.style.width = "70%";
-    art2.style.width = "70%";
     art3.style.width = "70%";
   } else if (scrollY >= 0) {
     art1.style.width = "0%";
-    art2.style.width = "0%";
     art3.style.width = "0%";
   }
 }
 
-function thembtn() {
-  var element = document.body;
-  element.classList.toggle("dark-mode");
-}
 
- 
+
+
+function myFunction(x) {
+  let side = document.querySelector(".side");
+  x.classList.toggle("change");
+  side.classList.toggle("side-open");
+}
